8e97aee281ecfb4c50f90cb9560315c2ca955397
hamza887 <hamzabutt887>
2021-07-06 01:02:26 -0400

    downloading dependecies

--- pokemon-search-engine/src/App.js
+++ pokemon-search-engine/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
  
   return (
     <div className="App">
-    
+      <h1>hello</h1>
+      <Navbar/>
+
     </div>
   );
 }
